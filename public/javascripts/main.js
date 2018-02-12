5b2c3b5a341a64bac04915f1fae66c0f2b16a76b
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-02-12 20:03:28 +0100

    geolocation done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,6 +11,41 @@ function startMap () {
       center: ironhackBCN
     }
   );
+
+  if (navigator.geolocation) {
+    // Get current position
+    // The permissions dialog will popup
+    navigator.geolocation.getCurrentPosition(function (position) {
+      // Create an object to match
+      const center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      // Center the map in the position we got
+      const myMarker = new google.maps.Marker({
+        position: {
+          lat: center.lat,
+          lng: center.lng
+        },
+        map: map,
+        title: "I'm here"
+      });
+    }, function () {
+      // If something else goes wrong
+      console.log('Error in the geolocation service.');
+    });
+  } else {
+    // Browser says: Nah! I do not support this.
+    // console.log('Browser does not support geolocation.');
+    const myMarker = new google.maps.Marker({
+      position: {
+        lat: 41.3977381,
+        lng: 2.190471916
+      },
+      map: map,
+      title: "I'm here"
+    });
+  }
 }
 
 startMap();
