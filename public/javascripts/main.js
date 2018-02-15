6fc8e5cf2af102f539f639eb7f5e3b348b0968fd
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-02-15 19:28:04 +0100

    custom labels for bars done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
 function main () {
-  const barname = myBars[0].barname;
-
   function initMap () {
     let myMarker;
     const mapOptions = {
@@ -32,8 +30,8 @@ function main () {
           };
 
           let icon = {
-            url: '../images/currentpositionmarker.png',
-            scaledSize: new google.maps.Size(20, 20)
+            url: '../images/blueicon.png',
+            scaledSize: new google.maps.Size(25, 25)
           };
 
           myMarker = new google.maps.Marker({
@@ -57,19 +55,29 @@ function main () {
     // --- lop to create markers for the bars location and infoWindow
     // --- create infoWindow with the content
     for (let i = 0; i < myBars.length; i++) {
-      let contentString = myBars[i].barname;
+      let contentString = '<strong>' + myBars[i].barname + '</strong></br>' + myBars[i].address;
       const infoWindow = new google.maps.InfoWindow(
         {content: contentString});
 
         // --- create markers for each bar
+      let icon = {
+        url: '../images/coin-brown-border.png',
+        scaledSize: new google.maps.Size(31, 31)
+      };
       const barsMarker = new google.maps.Marker({
+
         position: {
           lat: myBars[i].location.coordinates[0],
           lng: myBars[i].location.coordinates[1]
         },
-        // --- icon: icon, place to change icon
+        icon: icon,
         map: map,
-        label: myBars[i].price.toString() + '€'
+        label: {
+          text: myBars[i].price.toString() + '€',
+          color: '#966A39',
+          fontSize: '12px'
+        }
+        // label: myBars[i].price.toString() + '€'
       });
 
       // --- bind each infowindow with each bar´s marker and add event listener to display and hide the infowindow
