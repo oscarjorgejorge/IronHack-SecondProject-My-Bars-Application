29b47349df77c74719f16a2a8edf902451aa5b63
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-02-15 13:20:26 +0100

    edit functionality working perfectly

--- routes/bar.js
+++ routes/bar.js
@@ -60,52 +60,32 @@ router.post('/profile', (req, res, next) => {
     return res.redirect('/');
   }
 
+  // --- Colect data from the edit form
+  const barname = req.body.barname;
   const originalPrice = req.body.price;
-  const updates = {
-    username: req.body.username,
-    barname: req.body.barname,
-    price: Math.round(originalPrice * 10) / 10,
-    address: req.body.address,
-    location: {
-      type: 'Point',
-      coordinates: []
-    }
+  let price = Math.round(originalPrice * 10) / 10;
+  const address = req.body.address;
+  const location = { type: 'Point',
+    coordinates: []
   };
+
+  const updates = {
+    barname,
+    price,
+    address,
+    location
+  };
+
   const id = req.session.currentUser._id;
-  // const username = req.body.username;
-  // const barname = req.body.barname;
 
-  // let price = Math.round(originalPrice * 10) / 10;
-
-  // // --- address using with geocoder
-  // const address = req.body.address;
-  // let location = {
-  //   coordinates: []
-  // };
-
-  // --- data validation for edit
-  if (updates.username === '' || updates.barname === '' || originalPrice === '' ||
-  updates.address === '') {
+  // --- data validation for edit form
+  if (barname === '' || originalPrice === '' ||
+  address === '') {
     const data = {
       message: 'Try again'
     };
     return res.render('/bar/profile/' + id + '/edit', data);
   }
-
-  // edited res.render
-
-  // --- check if new username already exists
-  // Bar.findOne({ 'username': updates.username }, (err, user) => {
-  //   if (err) {
-  //     return next(err);
-  //   }
-  //   if (user && user.username !== updates.username) {
-  //     const data = {
-  //       message: 'The "' + updates.username + '" username is taken'
-  //     };
-  //     return res.render('/bar/profile/' + id + '/edit', data);
-  //   }
-  // });
 
   // --- optional settings for geocoder
   const options = {
@@ -117,69 +97,26 @@ router.post('/profile', (req, res, next) => {
 
     // -- transform address into latitude and longitude, optional settings, no needed
   const geocoder = NodeGeocoder(options);
-  geocoder.geocode(updates.address)
+  geocoder.geocode(address)
     .then((result) => {
-      updates.location.coordinates.push(Number(result[0].latitude));
-      updates.location.coordinates.push(Number(result[0].longitude));
-      console.log('updates inside the then etc etc...: ', updates);
-      // --- create and save new model in database, inside of geocode for asynchronous js
-      // const newBar = new Bar({
-      //   username,
-      //   barname,
-      //   price,
-      //   location,
-      //   address
-      // });
-
-      // const updates = {
-      //   username,
-      //   barname,
-      //   price,
-      //   address,
-      //   location
-      // };
+      location.coordinates.push(Number(result[0].latitude));
+      location.coordinates.push(Number(result[0].longitude));
     })
+    // --- update database and update information in session.currentUser
     .then(() => {
-      Bar.findByIdAndUpdate(id, updates, (err, paquito) => {
+      Bar.findByIdAndUpdate(id, updates, {new: true}, (err, newInfo) => {
+        console.log(newInfo);
         if (err) {
           return next(err);
         }
+        req.session.currentUser = newInfo;
+
         return res.redirect('/bar/profile');
-      });
-
-      // newBar.save((error) => {
-      //   if (error) {
-      //     return next(error);
-      //   }
-      //   // --- storing the new bar in session
-      //   req.session.currentUser = newBar;
-      //   console.log(req);
-      //   res.redirect('/bar/profile');
-      // });
-    })
-    .then(() => {
-      Bar.findOne({ 'username': updates.username }, (err, bar) => {
-        if (err) {
-          return next(err);
-        }
-        req.session.currentUser = bar;
-        res.redirect('/bar/profile');
-
-        // --- check if the password is correct
-        // if (bcrypt.compareSync(password, bar.password)) {
-        //   res.redirect('/bar/profile');
-        // } else {
-        //   const data = {
-        //     message: 'Password is incorrect'
-        //   };
-        //   res.render('auth/login', data);
-        // }
       });
     })
     .catch((err) => {
       console.log(err);
     });
 });
-// });
 
 module.exports = router;
